881d7d63c692319b6886c1b9652a4bc52d6ff27e
Balaji <rajanbalaji2000@yahoo.co.in>
2015-08-27 17:36:21 +0530

    Spring Core initial commit

---
+++ Spring.Core/src/main/java/spring/main/Properties.java
@@ -0,0 +1,15 @@
+package spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Properties
+{
+
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+	}
+
+}
